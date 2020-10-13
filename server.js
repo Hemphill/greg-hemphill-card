042dd8a8c869a0e8da4e037966472815b57f3f88
Greg Hemphill <greg@webstop.com>
2020-10-12 20:29:51 -0400

    fixes img build

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3100
 
 
-app.get('/', (req, res) => res.sendFile('docs/index.html'))
+app.get('/', (req, res) => res.sendFile('code/docs/index.html'))
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
